70caf22ee5d816890f2c3198faecd1ba3515653c
ercan <ercan@ERCAN>
2025-04-28 23:10:53 +0300

    Proje dosyası ekle.

---
+++ ehliyetUygunluguKontrolu/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ehliyetUygunluguKontrolu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.Write("Lütfen Yaşınızı Giriniz:");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.Write("Sağlık Durumunuz iyi mi? ( evet/hayır):");
+            string saglik = Console.ReadLine().ToLower().Trim();
+
+
+            Console.Write("Sabıkanız var mı? (evet/hayır):");
+            string sabika = Console.ReadLine().ToLower().Trim();
+
+
+            if (yas >= 18 && saglik == "evet" && sabika == "hayır")
+
+
+            {
+                Console.WriteLine("Ehliyeti Alabilirsiniz");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Ehliyeti alamazsınız");
+            }
+            
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
